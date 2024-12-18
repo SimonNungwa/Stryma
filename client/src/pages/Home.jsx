1e20a102c6a9da80d9f7f259ba14fc7cf1ad6b37
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:39:36 +0100

    Refactored home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import MobileNav from "../component/MobileNav";
+
+const Home = () => {
+    return (
+        <div className="p-4">
+            <h1>Home Page</h1>
+            <MobileNav />
+        </div>
+    );
+};
+
+export default Home;
