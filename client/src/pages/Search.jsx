04d561aff96ebccc43e11bfcf365b7008a8e4221
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:36:16 +0100

    code stub for search page

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import MobileNav from "../component/MobileNav";
+
+const Search = () => {
+    return (
+        <div className="p-4">
+            <h1>Search Page</h1>
+            <MobileNav />
+        </div>
+    );
+};
+
+export default Search;
