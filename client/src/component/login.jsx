fc5209789352644a4df031980051e3f7d5f8591e
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:31:44 +0100

    uppdate login component

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -1,13 +1,44 @@
-import {useState} from "react";
-
 const Login = () => {
     return (
-        <>
-            <form>
+        <div className="p-6 rounded-lg max-w-md mx-auto">
+            <form className="space-y-4">
+                <div>
+                    <label className="block text-gray-700 font-medium mb-1" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="w-full px-4 py-2 border-b border-black"
+                        placeholder="Enter your email"
+                        required
+                    />
+                </div>
 
+                <div>
+                    <label className="block text-gray-700 font-medium mb-1" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="w-full px-4 py-2 border-b border-black outline-none"
+                        placeholder="Enter your password"
+                        required
+                    />
+                </div>
+
+                <div className="text-lg">Forgot password?</div>
+
+                <button
+                    type="submit"
+                    className="w-full bg-black text-white py-4 rounded-md  "
+                >
+                    Login
+                </button>
             </form>
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default Login;
