9e816fe3f6abdbcc32896d7e99f5cdbc374882ae
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:37:11 +0100

    added page for saved songs or podcasts

---
+++ client/src/pages/Saved.jsx
@@ -0,0 +1,12 @@
+import MobileNav from "../component/MobileNav";
+
+const Saved = () => {
+    return (
+        <div className="p-4">
+            <h1>Saved Page</h1>
+            <MobileNav />
+        </div>
+    );
+};
+
+export default Saved;
