bdbfb9741b6312c87125810c4d4baf9f864c346e
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:37:43 +0100

    Added a navigation bar for small screens

---
+++ client/src/component/MobileNav.jsx
@@ -0,0 +1,72 @@
+import { AiOutlineHome, AiOutlineSearch, AiOutlineHeart, AiOutlineUser } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+
+const MobileNav = () => {
+    return (
+        <section className="fixed bottom-0 left-0 w-full bg-white shadow-xl md:hidden">
+            <ul className="flex justify-around items-center py-3">
+                {/* Home */}
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `flex flex-col items-center ${
+                                isActive ? "text-black font-semibold" : "text-gray-500"
+                            }`
+                        }
+                    >
+                        <AiOutlineHome size={24} />
+                        <span>Home</span>
+                    </NavLink>
+                </li>
+
+                {/* Search */}
+                <li>
+                    <NavLink
+                        to="/Search"
+                        className={({ isActive }) =>
+                            `flex flex-col items-center ${
+                                isActive ? "text-black font-semibold" : "text-gray-500"
+                            }`
+                        }
+                    >
+                        <AiOutlineSearch size={24} />
+                        <span>Search</span>
+                    </NavLink>
+                </li>
+
+                {/* Saved */}
+                <li>
+                    <NavLink
+                        to="/Saved"
+                        className={({ isActive }) =>
+                            `flex flex-col items-center ${
+                                isActive ? "text-black font-semibold" : "text-gray-500"
+                            }`
+                        }
+                    >
+                        <AiOutlineHeart size={24} />
+                        <span>Saved</span>
+                    </NavLink>
+                </li>
+
+                {/* Account */}
+                <li>
+                    <NavLink
+                        to="/Account"
+                        className={({ isActive }) =>
+                            `flex flex-col items-center ${
+                                isActive ? "text-black font-semibold" : "text-gray-500"
+                            }`
+                        }
+                    >
+                        <AiOutlineUser size={24} />
+                        <span>Account</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default MobileNav;
