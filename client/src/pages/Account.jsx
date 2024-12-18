c08e03fee137b892e625d0e8d7dd2cbd6736e903
SimonNungwa <simonnungwa99@gmail.com>
2024-12-18 18:40:10 +0100

    added code stub for account page

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,12 @@
+import MobileNav from "../component/MobileNav";
+
+const Account = () => {
+    return (
+        <div className="p-4">
+            <h1>Account Page</h1>
+            <MobileNav />
+        </div>
+    );
+};
+
+export default Account;
