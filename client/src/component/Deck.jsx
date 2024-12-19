379ff4fa6b09a94e13bead3ddb99e463285669a0
SimonNungwa <simonnungwa99@gmail.com>
2024-12-19 16:55:15 +0100

    added code stub for deck component

---
+++ client/src/component/Deck.jsx
@@ -0,0 +1,11 @@
+const Deck = () => {
+    return (
+        <>
+            <section className="fixed bottom-0 ">
+                Deck
+            </section>
+        </>
+    )
+}
+
+export default Deck;
