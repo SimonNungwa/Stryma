5f600ccbf58daf4c18b0cb5f5b12b69f63b8f343
SimonNungwa <simonnungwa99@gmail.com>
2024-12-19 16:54:12 +0100

    updated search page -> added categories component

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,10 +1,12 @@
 import MobileNav from "../component/MobileNav";
 import SearchBar from "../component/SearchBar.jsx";
+import Categories from "../component/Categories.jsx";
 
 const Search = () => {
     return (
         <div className="p-4">
             <SearchBar />
+            <Categories />
             <MobileNav />
         </div>
     );
