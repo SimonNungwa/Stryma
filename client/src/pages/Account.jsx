f3480f4b0ed5c4ad4d1df3d6bc921965c2125f62
Simon <simonnungwa99@gmail.com>
2025-01-18 04:06:30 +0100

    Account component

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -4,6 +4,23 @@ const Account = () => {
     return (
         <div className="p-4">
             <h1>Account Page</h1>
+
+            <section>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </section>
+
             <MobileNav />
         </div>
     );
