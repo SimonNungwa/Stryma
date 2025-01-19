4b396b14f387d8bce6ebc0ec763f309a50b7b839
Simon <simonnungwa99@gmail.com>
2025-01-19 23:58:36 +0100

    Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import MobileNav from "../component/MobileNav";
+import LikedSongs from "./LikedSongs.jsx";
+import Playlists from "./Playlists.jsx";
 
 const Home = () => {
     return (
         <div className="p-4">
             <h1>Home Page</h1>
+            <LikedSongs />
+            <Playlists />
             <MobileNav />
         </div>
     );
