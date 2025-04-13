a24fb6148d40d90c7c2b5b8547917cd5e1ce75a7
Simon <simonnungwa99@gmail.com>
2025-04-13 19:10:34 +0100

    updated navbar

---
+++ client/src/component/TopNav.jsx
@@ -0,0 +1,18 @@
+const TopNav = () => {
+    return (
+        <>
+            <nav className="flex">
+                <ul className="flex text-white">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li>Acc</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+
+export default TopNav;
